343a6d93445d6b8622708e17b0d60e59efe29048
Willem Toorop <willem@nlnetlabs.nl>
2018-06-29 00:09:55 +0200

    Code coverage per branch

--- mkindex.sh
+++ mkindex.sh
@@ -5,18 +5,23 @@ cat >index.html << HEAD
 <head><title>Example ITHI Pages</title></head>
 <body>
   <ul>
-    <li><a href="coverage">Results from coverage testing</a></li>
-    <li><a href="doxygen">Doxygen Documentation</a></li>
 HEAD
+for srcdir in `find coverage -type d -name src`
+do
+	reportdir=${srcdir%/src}
+	branch=${reportdir#coverage/}
+    	echo "    <li><a href=\"${reportdir}/\">Code coverage on branch ${branch}</a></li>" >> index.html
+done
 if [ -d analysis ]
 then
 	for r in analysis/*
 	do
 		name=${r%-*-*}
-    		echo "    <li><a href=\"$r\">Static analysis from ${name#analysis/}</a></li>" >>index.html
+    		echo "    <li><a href=\"$r\">Static analysis from ${name#analysis/}</a></li>" >> index.html
 	done
 fi
 cat >>index.html << TAIL
+    <li><a href="doxygen">Doxygen Documentation</a></li>
   </ul>
 </body>
 </html>
